8e2086336fc2ffd0982e0fbc0fcd46081d482fcf
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2023-01-12 15:31:53 +0100

    Add very simple test case

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'simple#index'
+  get '/redirect' => 'simple#do_redirect', as: :do_redirect
+
   mount Redirectr::Engine => "/redirectr"
 end
--- test/integration/navigation_test.rb
+++ test/integration/navigation_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class NavigationTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'basic stuff' do
+    # Test referrer param
+    get '/redirect?referrer=http://www.example.com/?foo=bar'
+    follow_redirect!
+    assert_equal 'http://www.example.com/?foo=bar', request.url
+
+    # Test `ApplicationController#default_url` method
+    get '/redirect'
+    follow_redirect!
+    assert_equal 'http://www.example.com/?this_is_default_url=1', request.url
+
+    # Test `default` parameter for `back_or_default`
+    get '/redirect?other_default=http://www.example.com/?other_default=1'
+    follow_redirect!
+    assert_equal 'http://www.example.com/?other_default=1', request.url
+  end
 end
